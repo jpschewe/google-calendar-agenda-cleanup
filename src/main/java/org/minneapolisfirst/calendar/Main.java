a898d6ca51d338b7fd75e962259342a81fd6a5b3
Jon Schewe <jpschewe@mtu.net>
2018-08-16 20:50:02 -0500

    Working example

--- src/main/java/org/minneapolisfirst/calendar/Main.java
+++ src/main/java/org/minneapolisfirst/calendar/Main.java
@@ -12,18 +12,19 @@ import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.geom.LineSegment;
-import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.geom.Vector;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfReader;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.kernel.pdf.canvas.parser.EventType;
 import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
 import com.itextpdf.kernel.pdf.canvas.parser.data.IEventData;
 import com.itextpdf.kernel.pdf.canvas.parser.data.TextRenderInfo;
-import com.itextpdf.kernel.pdf.canvas.parser.filter.IEventFilter;
 import com.itextpdf.kernel.pdf.canvas.parser.listener.IEventListener;
 
 public class Main {
@@ -34,7 +35,7 @@ public class Main {
 		final Path src = Paths.get("/home/jpschewe/Downloads/calendar_2018-08-04_2018-08-19.pdf");
 		final Path dest = Paths.get("/home/jpschewe/Downloads/calendar_clean.pdf");
 
-		final Main app = new Main(src, dest);
+		new Main(src, dest);
 
 	}
 
@@ -42,7 +43,6 @@ public class Main {
 
 		try (PdfDocument srcDoc = new PdfDocument(new PdfReader(src.toFile()));
 				PdfDocument destDoc = new PdfDocument(new PdfWriter(dest.toFile()))) {
-			final Rectangle pageSize = srcDoc.getFirstPage().getPageSize();
 
 			for (int i = 1; i <= srcDoc.getNumberOfPages(); ++i) {
 				final PdfPage page = srcDoc.getPage(i);
@@ -51,36 +51,87 @@ public class Main {
 				final PdfCanvasProcessor processor = new PdfCanvasProcessor(gatherBaselines);
 				processor.processPageContent(page);
 
-				LOGGER.info("Filter baselines for page {} -> {}", i, gatherBaselines.baselinesToFilter);
+				LOGGER.debug("Filter baselines for page {} -> {}", i, gatherBaselines.baselinesToFilter);
 
-				destDoc.setDefaultPageSize(new PageSize(pageSize));
-				destDoc.addNewPage();
+				final PdfPage destPage = page.copyTo(destDoc);
+				final FilterEventsByBaseline filterRects = new FilterEventsByBaseline(gatherBaselines.baselinesToFilter,
+						destPage);
+				final PdfCanvasProcessor processor2 = new PdfCanvasProcessor(filterRects);
+				processor2.processPageContent(destPage);
+
+				destDoc.addPage(destPage);
 			}
 
 		}
 	}
 
-	public class FilterEventsByBaseline implements IEventFilter {
+	public class FilterEventsByBaseline implements IEventListener {
 		private final List<Float> baselinesToFilter;
+		private final PdfPage page;
+		private final PdfCanvas canvas;
 
-		public FilterEventsByBaseline(final List<Float> baselinesToFilter) {
+		public FilterEventsByBaseline(final List<Float> baselinesToFilter, final PdfPage page) {
 			this.baselinesToFilter = baselinesToFilter;
+			this.page = page;
+			canvas = new PdfCanvas(this.page);
+		}
+
+		private class BoundingRect {
+			float minX = Float.MAX_VALUE;
+			float minY = Float.MAX_VALUE;
+			float maxX = 0;
+			float maxY = 0;
+
+			public void add(final LineSegment line) {
+				add(line.getStartPoint());
+				add(line.getEndPoint());
+			}
+
+			public void add(final Vector v) {
+				minX = Math.min(minX, v.get(0));
+				maxX = Math.max(maxX, v.get(0));
+
+				minY = Math.min(minY, v.get(1));
+				maxY = Math.max(maxY, v.get(1));
+			}
+
+			public Rectangle toRectangle() {
+				return new Rectangle(minX, minY, maxX - minX, maxY - minY);
+			}
+
+			@Override
+			public String toString() {
+				return String.format("%f, %f - %f, %f", minX, minY, maxX, maxY);
+			}
 		}
 
 		@Override
-		public boolean accept(final IEventData data, final EventType type) {
+		public void eventOccurred(final IEventData data, final EventType type) {
 			if (type.equals(EventType.RENDER_TEXT)) {
 				final TextRenderInfo renderInfo = (TextRenderInfo) data;
 				final LineSegment baseline = renderInfo.getBaseline();
 				final float checkY = baseline.getStartPoint().get(1);
 
 				final boolean filter = baselinesToFilter.stream().anyMatch(f -> Math.abs(checkY - f) < 1E-6);
-				return !filter;
+				if (filter) {
+					final BoundingRect bounds = new BoundingRect();
+					bounds.add(renderInfo.getAscentLine());
+					bounds.add(renderInfo.getDescentLine());
+					bounds.add(renderInfo.getBaseline());
+
+					LOGGER.debug("Filter {}", bounds);
+
+					canvas.setStrokeColor(ColorConstants.WHITE).setFillColor(ColorConstants.WHITE)
+							.rectangle(bounds.toRectangle()).fillStroke();
+				}
 			}
-
-			return true;
-
 		}
+
+		@Override
+		public Set<EventType> getSupportedEvents() {
+			return Collections.singleton(EventType.RENDER_TEXT);
+		}
+
 	}
 
 	public class GatherBaselines implements IEventListener {
@@ -104,7 +155,7 @@ public class Main {
 			}
 
 		}
-		
+
 		@Override
 		public Set<EventType> getSupportedEvents() {
 			return Collections.singleton(EventType.RENDER_TEXT);
